6356491407c20d9f4526c7cdefb4460a2aacc07d
ariantchupan <atchupan@gmail.com>
2023-10-28 13:40:54 +0330

    vv

---
+++ Services/Customers/Customers.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Customers.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Services/Customers/Customers.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Customers.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
