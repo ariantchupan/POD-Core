3f9b69af5e3d20317671dae90c9776441e1d8a9b
ariantchupan <atchupan@gmail.com>
2023-10-28 13:37:15 +0330

    ss

---
+++ Services/Commerce/Commerce.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Commerce.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Services/Commerce/Services.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Services.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
