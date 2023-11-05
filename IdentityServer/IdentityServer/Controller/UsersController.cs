d918838ee573b89c87df0ab9fd5bed452f5513da
Arian <atchupan@gmail.com>
2023-11-05 20:24:22 +0330

    update

---
+++ IdentityServer/IdentityServer/Controller/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Controller
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+        //[HttpPost(Name = "SMSVerify")]
+        //[ProducesResponseType(StatusCodes.Status204NoContent)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesDefaultResponseType]
+        //public async Task<ActionResult> SMSVerify([FromBody] SendVerifyCommand command)
+        //{
+        //    return Ok(await _mediator.Send(command));
+        //}
+    }
+}
